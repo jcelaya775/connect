a94b3653cf20a934f1815349a3e1fab506c86a35
zachary belles <zmbelles@noctrl.edu>
2023-02-11 13:15:41 -0600

    finished PUT POST DELETE for user

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -5,16 +5,16 @@ import { hashPassword, comparePassword } from "@/validation/passwordHash";
 import { userValidationSchema } from "@/validation/userValidation";
 
 type Data = {
-	name?: string;
-	success: boolean;
-	data?: IUser[];
+  name?: string;
+  success: boolean;
+  message: string;
+  data?: IUser[];
 };
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-<<<<<<< HEAD
   const { method } = req;
 
   await connectDB();
@@ -25,12 +25,12 @@ export default async function handler(
       //make sure the username follows conventions
       const validationResult = userValidationSchema.validate(req.body.username);
       if (validationResult.error) {
-        return res.status(400).json({ success: false });
+        return res.status(400).json({ success: false, message: "Data doesnt follow conventions" });
       }
       try {
         //find user with username matching req
-        let username = req.body.username
-        let thisUser = await User.findOne({ username });
+        let email = req.body.email
+        let thisUser = await User.findOne({ email });
         
         //hash the attempted password
         let attemptedPassHash = await hashPassword(req.body.password);
@@ -41,67 +41,70 @@ export default async function handler(
 
         //if the passwords are the same, return the user
         if (isCorrectPassword) {
-          res.status(200).json({ success: true, data: thisUser });
+          res.status(200).json({ success: true, data: thisUser, message: "User found"});
         } 
         else {
           //unsure what error status to return upon failed password verification
-          res.status(401).json({ success: false });
+          res.status(401).json({ success: false, message: "Incorrect username or password"});
         }
       } 
       catch (error) {
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, message: "Something went wrong"});
       }
       break;
     case "POST":
-      // try {
-      //   const validationResult = userValidationSchema.validate(req.body.username);
-      //   if (validationResult.error) {
-      //     return res.status(400).json({ success: false });
-      //   }
-      //   const { name, email, password, username } = req.body; 
-      //   const hashedPassword = await hashPassword(req.body.password);
-      //   const user = new User({
-      //     username: req.body.username,
-      //     email: req.body.email,
-      //     password: hashedPassword,
-      //   });
-      //   await user.save();
-      //   res.status(201).json({ success: true });
-      // } catch (error) {
-      //   res.status(400).json({ success: false });
-      // }
-      // break;
+      try {
+        const validationResult = userValidationSchema.validate(req.body.username);
+        if (validationResult.error) {
+          return res.status(400).json({ success: false, message: "Incorrect username or password"});
+        }
+        // add code to check if email already exists
+        const hashedPassword = await hashPassword(req.body.password);
+        const user = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword,
+          verified: false
+        });
+        await user.save();
+        res.status(201).json({ success: true, message: "User created"});
+      } catch (error) {
+        res.status(400).json({ success: false, message: "Something went wrong"});
+      }
+      break;
     case "PUT":
+      try {
+        const { _id, ...updateData } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(_id, updateData, {
+          new: true, //returns the updated user
+        });
+  
+        if (!updatedUser) {
+          return res.status(404).json({ success: false, message: "User not found" });
+        }
+  
+        return res.status(200).json(updatedUser);
+      } 
+      catch (error) {
+        return res.status(500).json({ success: false, message: "something went wrong" });
+      }
+    
       break;
     case "DELETE":
+      try {
+        const { _id } = req.query;
+    
+        await User.findByIdAndDelete(_id);
+    
+        // Return a success response
+        res.status(200).json({ success: true, message: 'User deleted successfully' });
+      } catch (error) {
+        // Return an error response
+        res.status(500).json({ success: false, message: "User not Found" });
+      }
       break;
     default:
-      res.status(400).json({ success: false });
+      res.status(400).json({ success: false, message: "Method not allowed"});
       break;
   }
-=======
-	const { method } = req;
-	console.log(`method: ${method}`);
-	await connectDB();
-	switch (method) {
-		case "GET":
-			try {
-				const users: IUser[] = await User.find({});
-				res.status(200).json({ success: true, data: users });
-				console.log(users);
-			} catch (error) {
-				res.status(400).json({ success: false });
-			}
-			break;
-		case "POST":
-			break;
-		case "PUT":
-			break;
-		case "DELETE":
-			break;
-		default:
-			res.status(400).json({ success: false });
-			break;
-	}
->>>>>>> develop
 }
