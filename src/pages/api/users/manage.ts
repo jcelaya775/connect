39ad1459e54587f47a7692f33ceb46695248eebc
Zachary Belles <zmbelles@noctrl.edu>
2023-02-23 10:27:39 -0600

    added api/users/manage to get, edit or delete user

--- src/pages/api/users/manage.ts
+++ src/pages/api/users/manage.ts
@@ -5,8 +5,8 @@ import { hashPassword, comparePassword } from "@/validation/passwordHash";
 import { userValidationSchema } from "@/validation/userValidation";
 
 type Data = {
-	name?: string;
 	success: boolean;
+	error?: string;
 	data?: IUser[];
 };
 
@@ -47,44 +47,32 @@ export default async function handler(
 					res.status(200).json({ success: true, data: thisUser });
 				} else {
 					//unsure what error status to return upon failed password verification
-					res.status(401).json({ success: false });
+					res.status(400).json({ success: false });
 				}
-			} catch (error) {
-				res.status(400).json({ success: false });
-			}
-			break;
-		case "POST":
-			try {
-				//make sure email follows conventions
-				const validationResult = userValidationSchema.validate(req.body.email);
-				if (validationResult.error) {
-					return res.status(400).json({ success: false });
-				}
-				//store the data into const's
-				const { name, email, password, username } = req.body;
-				const hashedPassword = await hashPassword(req.body.password);
-				let vCode = Math.random() * (99999 - 11111) + 11111;
-
-				//create the wire frame user
-				const wireUser = new User({
-					username: req.body.username,
-					email: req.body.email,
-					code: vCode,
-					password: hashedPassword,
-				});
-
-				//save the user
-				await wireUser.save();
-
-				//send the verification email and success status
-				res.status(201).json({ success: true });
 			} catch (error) {
 				res.status(400).json({ success: false });
 			}
 			break;
 		case "PUT":
+            const { username, name, email } = req.body;
+            try{
+                await User.findOneAndUpdate(username, name, email);
+                res.status(400).json({ success: true })
+            }
+            catch (error) {
+                res.status(400).json({ success: false });
+            }
 			break;
 		case "DELETE":
+			try{	
+				const { username, password } = req.body;
+				const pass_attempt = hashPassword(password)
+				await User.findOneAndDelete({ username: username, password: pass_attempt});
+				res.status(200).json({ success: true})
+			}
+			catch(error){
+				res.status(404).json({ success: false, error: "incorrect username or password" });
+			}
 			break;
 		default:
 			res.status(400).json({ success: false });
