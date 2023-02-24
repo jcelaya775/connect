5f4fd2e1da563a849a11336fab66ded333d1cb7c
zachary belles <zmbelles@noctrl.edu>
2023-02-24 09:59:46 -0600

    no more findanddelete, added pass verification

--- src/pages/api/users/manage.ts
+++ src/pages/api/users/manage.ts
@@ -6,8 +6,8 @@ import { userValidationSchema } from "@/validation/userValidation";
 
 type Data = {
 	success: boolean;
-	error?: string;
-	data?: IUser[];
+	status: string;
+	data?: IUser;
 };
 
 export default async function handler(
@@ -20,21 +20,20 @@ export default async function handler(
 
 	switch (method) {
 		case "GET":
-			//make sure the username follows conventions
-			const validationResult = userValidationSchema.validate(req.body.username);
-			if (validationResult.error) {
-				return res.status(400).json({ success: false });
-			}
 			try {
 				//find user with username matching req
-				let username = req.body.username;
-				let thisUser = await User.findOne({ username });
-				const { query } = req;
-				if ("code" in query) {
+				let email = req.body.email;
+				const user: IUser | null = await User.findOne({ email });
+				if (!user) {
+					res
+						.status(404)
+						.json({ success: false, status: "Not Found" });
+					break;
 				}
+
 				//hash the attempted password
 				let attemptedPassHash = await hashPassword(req.body.password);
-				let userPassHash = thisUser.pass_hash;
+				let userPassHash = user.password;
 
 				//compare the hashes
 				let isCorrectPassword = await comparePassword(
@@ -44,38 +43,82 @@ export default async function handler(
 
 				//if the passwords are the same, return the user
 				if (isCorrectPassword) {
-					res.status(200).json({ success: true, data: thisUser });
+					res.status(200).json({ success: true, data: user, status: "OK" });
+					break;
 				} else {
-					//unsure what error status to return upon failed password verification
-					res.status(400).json({ success: false });
+					res.status(403).json({ success: false, status: "Forbidden" });
+					break;
 				}
-			} catch (error) {
-				res.status(400).json({ success: false });
+			} 
+			catch (error) {
+				res.status(400).json({ success: false, status: "Bad Request" });
 			}
 			break;
 		case "PUT":
-            const { username, name, email } = req.body;
-            try{
-                await User.findOneAndUpdate(username, name, email);
-                res.status(400).json({ success: true })
-            }
-            catch (error) {
-                res.status(400).json({ success: false });
-            }
-			break;
-		case "DELETE":
+			// const { username, name, email } = req.body;
+			// try{
+			// 		//unsure how to change this from findOneAndUpdate
+			// 		await User.findOneAndUpdate(username, name, email);
+			// 		res.status(200).json({ success: true, status: "OK" })
+			// 		break;
+			// }
+			// catch (error) {
+			// 		res.status(500).json({ success: false, status: "Internal Server Error" });
+			// }
+			// break;
+
 			try{	
-				const { username, password } = req.body;
-				const pass_attempt = hashPassword(password)
-				await User.findOneAndDelete({ username: username, password: pass_attempt});
-				res.status(200).json({ success: true})
+				const { email, password } = req.body;
+				
+				
+				const user: IUser | null = await User.findOne({ email });
+				if (!user) {
+					res
+						.status(404)
+						.json({ success: false, status: "Not Found" });
+					break;
+				}
+				
+				const pass_attempt = await hashPassword(password)
+				const isMatch = await comparePassword(pass_attempt, user.password);
+				if(!isMatch) {
+					res.status(403).json({ success: false, status: "Forbidden" })
+					break;
+				}
+				User.updateOne
+				res.status(200).json({ success: true, status: "OK" })
 			}
 			catch(error){
-				res.status(404).json({ success: false, error: "incorrect username or password" });
+				res.status(404).json({ success: false, status: "Not Found" });
+			}
+		case "DELETE":
+			try{	
+				const { email, password } = req.body;
+				
+				
+				const user: IUser | null = await User.findOne({ email });
+				if (!user) {
+					res
+						.status(404)
+						.json({ success: false, status: "Not Found" });
+					break;
+				}
+				
+				const pass_attempt = await hashPassword(password)
+				const isMatch = await comparePassword(pass_attempt, user.password);
+				if(!isMatch) {
+					res.status(403).json({ success: false, status: "Forbidden" })
+					break;
+				}
+				User.deleteOne(email);
+				res.status(200).json({ success: true, status: "OK" })
+			}
+			catch(error){
+				res.status(404).json({ success: false, status: "Not Found" });
 			}
 			break;
 		default:
-			res.status(400).json({ success: false });
+			res.status(500).json({ success: false, status: "Internal Server Error" });
 			break;
 	}
 }
