3608336a9ddcae1d18a5fdf4b2183e36b7088306
jrfleming <jrfleming@noctrl.edu>
2023-02-28 21:53:30 -0600

    axios added

--- src/pages/PostMessage.tsx
+++ src/pages/PostMessage.tsx
@@ -1,12 +1,34 @@
-import PostMessageForm from './PostMessageForm';
+import { useState } from 'react';
+import usePostMessage from './PostMessageForm';
 
-function App() {
+// Define a form component for posting messages
+const PostMessageForm = () => {
+  // Initialize a state for the message input value
+  const [message, setMessage] = useState('');
+  
+  // Get the `useMutation` instance for posting a message from the server
+  const postMessageMutation = usePostMessage();
+
+  // Define a function to handle the form submission
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Check if the message is not empty or only whitespace
+    if (message.trim() !== '') {
+      // Call the `mutateAsync` function to send the message to the server
+      await postMessageMutation.mutateAsync({ message });
+      // Reset the message input value
+      setMessage('');
+    }
+  };
+
+  // Render the form with the message input and a submit button
   return (
-    <div>
-      <PostMessageForm />
-      {/* Other components */}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
 
-export default App;
+export default PostMessageForm;
--- src/pages/PostMessageForm.tsx
+++ src/pages/PostMessageForm.tsx
@@ -1,132 +1,38 @@
-import { useState, useEffect } from "react";
-import { useMutation, useQueryClient } from "react-query";
-import { IMessageData } from "../models/Temp";
+import { useMutation, useQueryClient } from 'react-query';
+import axios from 'axios';
 
-// TODO: Later, we can use Axios
-// const getPostMessages = async () => {
-// 	const response = await fetch("/api/temp"); // GET reqeust by default
-// 	const data = await response.json();
-// 	return data;
-// };
+interface MessageData {
+  id: string;
+  message: string;
+}
 
-const createPostMessage = async (message: IMessageData) => {
-	console.log(message);
+interface PostMessageRequest {
+  message: string;
+}
 
-	const response = await fetch("/api/temp", {
-		method: "POST", // this is POST request
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ message }),
-	});
+// Define a custom hook for posting a message
+const usePostMessage = () => {
+  // Get a reference to the query client instance
+  const queryClient = useQueryClient();
 
-	return response;
+  // Define a mutation using the useMutation hook
+  const postMessageMutation = useMutation<MessageData, unknown, PostMessageRequest>(
+    // Define an async function that sends a POST request to the server to create a new message
+    async (request) => {
+      const response = await axios.post<MessageData>('/api/temp', request);
+      // Return the response data
+      return response.data;
+    },
+    {
+      // When the mutation succeeds, invalidate the 'messages' query in the query cache
+      onSuccess: () => {
+        queryClient.invalidateQueries('messages');
+      },
+    },
+  );
+
+  // Return the mutation object
+  return postMessageMutation;
 };
 
-// const updatePostMessage = async (message_id: string, message: IMessageData) => {
-// 	const response = await fetch("/api/temp", {
-// 		method: "PUT", // this is PUT request
-// 		headers: {
-// 			"Content-Type": "application/json",
-// 		},
-// 		body: JSON.stringify({
-// 			id: message_id,
-// 			message: message.message,
-// 		}), // here's where we specify the json request body
-// 	});
-
-// 	const data = await response.json();
-// 	console.log(data);
-
-// 	return data;
-// };
-
-// Define a custom hook that returns a `useMutation` instance
-// for posting a message to the server
-function usePostMessage(message: any) {
-	const queryClient = useQueryClient();
-
-	const postMessage = useMutation<IMessageData>(
-		async (message: any) => {
-			// Send a POST request to the server to create a new message
-			const response = await createPostMessage(message);
-
-			// Parse the JSON response from the server
-			const data = await response.json();
-
-			console.log(data);
-
-			// Return the response data
-			return data;
-		},
-		{
-			// When the mutation succeeds, invalidate the 'messages' query in the query cache
-			onSuccess: () => {
-				queryClient.invalidateQueries("messages");
-			},
-		}
-	);
-
-	return postMessage;
-}
-
-function PostMessageForm() {
-	// Initialize a state for the message input value
-	const [message, setMessage] = useState("");
-
-	// Get the `useMutation` instance for posting a message from the server
-	let postMessage = usePostMessage(message);
-
-	useEffect(() => {
-		postMessage = usePostMessage(message);
-	}, [message]);
-
-	// Define a function to handle the form submission
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-
-		// Call the `mutate` function to send the message to the server
-		postMessage.mutate();
-
-		// Reset the message input value
-		setMessage("");
-	};
-
-	return (
-		<form onSubmit={handleSubmit} style={{ marginBottom: 20 }}>
-			<label htmlFor="message" style={{ display: "block", marginBottom: 10 }}>
-				Post Message:
-			</label>
-			<textarea
-				id="message"
-				value={message}
-				onChange={(e) => {
-					console.log(e.target.value);
-					setMessage(e.target.value);
-				}}
-				style={{
-					display: "block",
-					marginBottom: 10,
-					width: "100%",
-					padding: 10,
-					borderRadius: 4,
-					border: "1px solid #ccc",
-				}}
-			/>
-			<button
-				type="submit"
-				style={{
-					background: "blue",
-					color: "#fff",
-					border: "none",
-					borderRadius: 4,
-					padding: "10px 20px",
-				}}
-			>
-				Post
-			</button>
-		</form>
-	);
-}
-
-export default PostMessageForm;
+export default usePostMessage;
