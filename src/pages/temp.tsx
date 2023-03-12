3dfce9de34c756a67b335985559f3e0f82151fe4
Jorge Celaya <jcelaya775@gmail.com>
2023-03-12 16:10:08 -0500

    Updated temp.tsx

--- src/pages/temp.tsx
+++ src/pages/temp.tsx
@@ -1,92 +1,40 @@
-import { useQuery } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import create from 'zustand';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { IPost } from "src/models/Post";
 
-// Define the shape of a message
-type Message = {
-  id: number;
-  title: string;
-  message: string;
-  likes: number;
+const PostsList = () => {
+	const [posts, setPosts] = useState<IPost[]>();
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			try {
+				const res = await axios.get("/api/posts");
+				console.log(res);
+				const postsData = res.data.data;
+				setPosts(postsData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchPosts();
+	}, []);
+
+	return (
+		<div>
+			<h1>Posts</h1>
+			{posts ? (
+				<ul>
+					{posts.map((post) => (
+						<li key={post._id}>
+							<h4>{post.title}</h4>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
 };
 
-// Define the shape of the state for the useMessageStore
-type State = {
-  messages: Message[];
-  setMessages: (messages: Message[]) => void;
-  addLike: (id: number) => void;
-};
-
-// Create the useMessageStore
-const useMessageStore = create<State>((set) => ({
-  messages: [], // Initial empty array of messages
-  setMessages: (messages) => set({ messages }), // Update messages array in state
-  addLike: (id) =>
-    set((state) => ({
-      messages: state.messages.map((message) =>
-        message.id === id ? { ...message, likes: message.likes + 1 } : message // Increment likes count for selected message
-      ),
-    })),
-}));
-
-export default function PostMessages() {
-  const { messages } = useMessageStore(); // Get messages from the store
-
-  // Fetch messages from server using react-query
-  const { isLoading, error } = useQuery<Message[]>(
-    'messages', // Cache key
-    async () => {
-      const response = await fetch('/api/temp'); // Fetch data from server
-      const data = await response.json(); // Parse response as JSON
-      const parsedMessages = data.message.map(
-        (message: { title: string; message: string }, idx: number) => ({
-          id: idx,
-          title: message.title,
-          message: message.message,
-          likes: 0, // Initialize likes count to 0
-        })
-      );
-      useMessageStore.getState().setMessages(parsedMessages); // Set messages in the store
-      return parsedMessages; // Return parsed messages
-    }
-  );
-
-  // Display loading message if data is still loading
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  // Display error message if there is an error
-  if (error) {
-    return <p>Oops! Something went wrong.</p>;
-  }
-
-  // Render the list of messages
-  return (
-    <div>
-      <h1>Post Messages</h1>
-      {messages &&
-        messages.map((message) => (
-          <div key={message.id}>
-            <h2>
-              <a
-                href={`/messages/${message.id}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {`Message ${message.id}`}
-              </a>
-            </h2>
-            <p>{message.message}</p>
-            <p>Likes: {message.likes}</p>
-            <button
-              onClick={() => useMessageStore.getState().addLike(message.id)} // Add like to selected message
-            >
-              Like
-            </button>
-          </div>
-        ))}
-      <ReactQueryDevtools position="bottom-right" />
-    </div>
-  );
-}
+export default PostsList;
