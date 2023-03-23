3940cb3dca1702e409b2c396b4a4cf3e43d414fa
zachary belles <zmbelles@noctrl.edu>
2023-03-23 11:10:08 -0500

    same thing i just tried

---
+++ src/pages/verify-email.jsx
@@ -0,0 +1,11 @@
+import VerifyBox from "@/components/verify-box";
+import Header from "@/components/Header";
+
+export default function Verify() {
+	return (
+		<>
+			<Header />
+			<VerifyBox />
+		</>
+	);
+}
