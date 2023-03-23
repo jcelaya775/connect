c67dde45e20e17afc2018f843607f69f0c453736
zachary belles <zmbelles@noctrl.edu>
2023-03-23 11:10:01 -0500

    edited header imports to work

---
+++ src/pages/password-reset.tsx
@@ -0,0 +1,13 @@
+import ResetBox from "@/components/Reset";
+import Header from "@/components/Header";
+
+const Reset = () => {
+	return (
+		<>
+			<Header />
+			<ResetBox />
+		</>
+	);
+};
+
+export default Reset;
