e49b1c28fc1e19982b2b09e5c2b9c285924788be
bjfyock <bjfyock@noctrl.edu>
2023-03-28 01:30:57 -0500

    profile page update

--- src/components/profile-page.tsx
+++ src/components/profile-page.tsx
@@ -1,18 +1,100 @@
 import React from "react";
 import SideNav from "./SideNav";
+import ConnectPost from "./ConnectPost"
+import SocialPost from "./SocialPost"
 
 export const ProfilePage = () => {
   return (
     <>
-      <div className="min-h-screen min-w-full bg-base-200 pt-16 pb-8">
+    <div className="min-h-screen min-w-full bg-base-200 pt-16 pb-8">
         <span className="hidden sm:block">
-          <SideNav />
+        <SideNav />
         </span>
         {/* Main Content Area */}
         <div className="flex-1 bg-base-200 min-h-screen pr-8 pt-6 pl-8 sm:pl-24 w-full lg:pl-48 xl:pr-0">
-          {/* Page content to be built here */}
+        <div className="flex flex-row gap-x-10 mt-5">
+        <div className="flex-initial w-full xl:w-3/4">
+        <div className="flex flex-col w-full gap-5">
+        <div className="bg-white overflow-hidden shadow rounded-md border-4 border-gray-500">
+        <div className="px-4 py-5 sm:p-6">
+          <div className="flex items-center">
+            <div className="avatar">
+              <div className="w-24 rounded">
+                <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="fill-current"><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c39.77 0 72 32.24 72 72S295.8 272 256 272c-39.76 0-72-32.24-72-72S216.2 128 256 128zM256 448c-52.93 0-100.9-21.53-135.7-56.29C136.5 349.9 176.5 320 224 320h64c47.54 0 87.54 29.88 103.7 71.71C356.9 426.5 308.9 448 256 448z"/></svg>
+              </div>
+            </div>
+            <div className="ml-4 text-center">
+              <h3 className="text-3xl font-medium leading-6 text-gray-900 mb-2">John Doe</h3>
+              <p className="text-xl font-medium text-gray-400 mb-5">Connect User Since 2023</p>
+              <p className="text-m font-meduium text-black mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, eligendi blanditiis ipsam repellat porro iure enim deleniti a. Ut facere tempora velit dolore excepturi voluptates ea autem provident repudiandae odio?</p>
+              <button className="btn gap-2 mr-5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" /></svg>
+                  16.1K Connect Friends
+              </button>
+            <button className="btn gap-2 mr-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            Create Post
+
+            </button>
+            <button className="btn gap-2 mr-5">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+              View Dashboard
+            </button>
+            <button className="btn gap-2 mr-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
+            </svg>
+            Add Goals
+            </button>
+            </div>
+          </div>
         </div>
-      </div>
+        </div>
+            <SocialPost />
+            <ConnectPost />
+            <ConnectPost />
+            <SocialPost />
+            <SocialPost />
+            <ConnectPost />
+            <SocialPost />
+            <ConnectPost />
+            <SocialPost />
+            <ConnectPost />
+            </div>
+            </div>
+            <div className="flex-2 pr-6 w-1/4 hidden xl:block box-border text-center">
+            <h3 className="text-center font-medium text-3xl mb-5">Dashboard Preview</h3>
+            <div className="stats stats-vertical shadow text-center">
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+              </div>
+              <div className="stat-title">Total Likes</div>
+              <div className="stat-value text-primary">25.6K</div>
+              <div className="stat-desc">21% more than last month</div>
+            </div>
+            
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+              </div>
+              <div className="stat-title">Page Views</div>
+              <div className="stat-value text-secondary">2.6M</div>
+              <div className="stat-desc">21% more than last month</div>
+            </div>
+            <div className="stat">
+            <div className="stat-figure text-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="lightgreen" className="w-8 h-8"><path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" /></svg>
+            </div>
+            <div className="stat-value">100%</div>
+            <div className="stat-title">Weekly Goals Achieved</div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
     </>
   )
 };
