02ae3f754be4295ea5774ef1488a5e188d7a6467
Jorge Celaya <jcelaya775@gmail.com>
2023-03-28 22:05:24 -0500

    Fix errors

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { useMutation } from "react-query";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { GetServerSidePropsContext } from "next/types";
+
+type CreatePostProps = {
+  onSuccess: () => void;
+};
+
+export default function CreatePost({ onSuccess }: CreatePostProps) {
+  const [visibility, setVisibility] = useState("public");
+  const [title, setTitle] = useState("");
+  const [community, setCommunity] = useState("");
+  const [content, setContent] = useState("");
+
+  const createPostMutation = useMutation(
+    async (postData: any) => {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error creating post");
+      }
+    },
+    {
+      onSuccess: () => {
+        onSuccess();
+      },
+    }
+  );
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    createPostMutation.mutate({
+      visibility,
+      title,
+      community,
+      content: {
+        body: content,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Visibility:
+          <select
+            value={visibility}
+            onChange={(e) => setVisibility(e.target.value)}
+          >
+            <option value="public">Public</option>
+            <option value="private">Private</option>
+          </select>
+        </label>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Community:
+          <input
+            type="text"
+            value={community}
+            onChange={(e) => setCommunity(e.target.value)}
+          />
+        </label>
+        <label>
+          Content:
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+CreatePost.Layout = "LoggedIn";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
