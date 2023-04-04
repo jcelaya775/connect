6d06c28a65240428821b45dcf15b724692aa32c5
Jorge Celaya <jcelaya775@gmail.com>
2023-04-04 16:27:01 -0500

    Fix logout button

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -144,7 +144,13 @@ const SideNav = () => {
               </Link>
             </li>
             <li className="hover-bordered">
-              <Link href="#">
+              <Link
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -159,15 +165,7 @@ const SideNav = () => {
                     d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                   />
                 </svg>
-                <span
-                  className="hidden lg:block"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signOut();
-                  }}
-                >
-                  Logout
-                </span>
+                <span className="hidden lg:block">Logout</span>
               </Link>
             </li>
           </ul>
