5921d1abb06c3249b62000e09a9b136e229dfe1b
zachary belles <zmbelles@noctrl.edu>
2023-04-06 09:58:50 -0500

    no more test

--- src/pages/api/posts/image/index.ts
+++ src/pages/api/posts/image/index.ts
@@ -1,17 +1,72 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import multiparty from "multiparty";
+import multerS3 from 'multer-s3';
+import multer from "multer";
 import connectDB from "@/lib/mongodb";
 import Post, { IPost } from "@/models/Post";
 import { getAuthUser } from "@/lib/auth";
-import { uploadHandler } from "@/lib/amazon-s3";
-import { request } from "http";
+import { S3Client } from '@aws-sdk/client-s3';
+import AWS from "aws-sdk";
 
-type GetData = {
-  success: boolean;
-  data?: IPost[];
-  error?: string;
-  email?: string;
-};
+//s3 instance for getSignedUrl()
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+//s3 client to upload using multers3
+const s3Client = new S3Client({
+  region: "your-region",
+  credentials: {
+    accessKeyId: "your-access-key-id",
+    secretAccessKey: "your-secret-access-key",
+  },
+});
+
+//function to parse form data
+async function parseFormData(
+  req: NextApiRequest & { files?: any },
+  res: NextApiResponse
+) {
+
+	//create the storage instance
+  const storage = multer.memoryStorage();
+
+	//attempt to create the upload instance
+  const multerUpload = multer({
+    storage: multerS3({
+      s3: s3Client,
+      bucket: "connectImagesAndVideos",
+      acl: 'public-read',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key: function (req, file, cb) {
+        cb(null, file.originalname);
+      },
+    }),
+  });
+	//create the file instance from the upload object
+  const multerFiles = multerUpload.any();
+
+	//create a promise to upload the file to the s3 bucket
+  await new Promise((resolve, reject) => {
+    multerFiles(req as any, res as any, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+
+  // Get the URL of the uploaded file
+  const url = s3.getSignedUrl("getObject", {
+    Bucket: "connectImagesAndVideos",
+    Key: req.files[0].key,
+  });
+
+  // Return the URL
+  return {
+    url: url,
+  };
+}
 
 // IMPORTANT: Prevents next from trying to parse the form
 export const config = {
@@ -19,7 +74,8 @@ export const config = {
     bodyParser: false,
   },
 };
-interface PostData {
+
+interface ReturnData {
   success: boolean;
   httpResponse: string;
   data?: string;
@@ -27,62 +83,24 @@ interface PostData {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<GetData | PostData>
+  res: NextApiResponse
 ) {
   const { method } = req;
-  console.log(method);
 
   await connectDB();
 
   switch (method) {
-    case "GET": // authenticated endpoint
-      try {
-        //authenticate the user
-        const user = await getAuthUser(req, res);
-        const { _id } = user!;
-        //get all the posts associated with their user_id
-        const posts: IPost[] = await Post.find<IPost>({ user_id: _id });
-
-        res.status(200).json({
-          success: true,
-          data: posts,
-        });
-      } catch (error: any) {
-        res.status(400).json({ success: false, error: error.message });
-      }
-
-      break;
-    /**
-     * @route   POST api/posts
-     * @desc    Create a post
-     * @access  Private
-     * @body    title: string (required)
-     * @body    community_id: string (required)
-     * @body    content: string (required)
-     * @body    visibility: string (required)
-     **/
-    case "POST": // authenticated endpoint
-      //authenticate the user
-      // const user = await getAuthUser(req, res);
-      // const { _id: user_id, username, email, name } = user!;
-      console.log(`POST api/posts`);
-      // Params
-      try {
-        
-        const { url } = await uploadHandler(req);
-        if(!url){
-          res.status(400).json({ success: false, httpResponse: "400" })
-        }
-        res.status(200).json({ success: true, httpResponse: "200", data: url });
-        //TODO: use the key and location to make the post with reference to s3
-      } catch (error) {
-        res.status(202).json({ success: false, httpResponse: "accepted but with errors" });
-      }
-
-    case "PUT":
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
+    case "GET": {
+      const data = req.query.data;
+      res.status(200).json({ success: true, httpStatus: "200", data: data });
+    }
+    case "POST":
+    {
+        const url = parseFormData(req, res);
+				if(!url){
+					res.status(400).json({ success: false, httpStatus: "Bad Request" });
+				}
+				res.status(201).json({ success: true, httpStatus: "Accepted", data: url})
+  	}
+	}
 }
