a0a5816f7dd0e9089948d6e815e7de978fd212c9
zachary belles <zmbelles@noctrl.edu>
2023-04-06 12:26:04 -0500

    new edits. only parser stops

--- src/pages/api/posts/image/index.ts
+++ src/pages/api/posts/image/index.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import multerS3 from 'multer-s3';
 import multer from "multer";
 import connectDB from "@/lib/mongodb";
-import Post, { IPost } from "@/models/Post";
 import { getAuthUser } from "@/lib/auth";
 import { S3Client } from '@aws-sdk/client-s3';
 import AWS from "aws-sdk";
@@ -15,10 +14,10 @@ const s3 = new AWS.S3({
 });
 //s3 client to upload using multers3
 const s3Client = new S3Client({
-  region: "your-region",
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: "your-access-key-id",
-    secretAccessKey: "your-secret-access-key",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "DEFAULT",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "DEFAULT",
   },
 });
 
@@ -30,12 +29,12 @@ async function parseFormData(
 
 	//create the storage instance
   const storage = multer.memoryStorage();
-
+  console.log("storage created");
 	//attempt to create the upload instance
   const multerUpload = multer({
     storage: multerS3({
       s3: s3Client,
-      bucket: "connectImagesAndVideos",
+      bucket: process.env.AWS_BUCKET_NAME || "DEFAULT",
       acl: 'public-read',
       contentType: multerS3.AUTO_CONTENT_TYPE,
       key: function (req, file, cb) {
@@ -43,43 +42,42 @@ async function parseFormData(
       },
     }),
   });
+  console.log("multerUpload instance created");
 	//create the file instance from the upload object
   const multerFiles = multerUpload.any();
-
+  console.log("multerFiles instance created");
 	//create a promise to upload the file to the s3 bucket
-  await new Promise((resolve, reject) => {
-    multerFiles(req as any, res as any, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
+  try{
+    await new Promise((resolve, reject) => {
+      multerFiles(req as any, res as any, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
     });
-  });
+    console.log("promise created");
+    // Get the URL of the uploaded file
+    const url = s3.getSignedUrl("getObject", {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: req.files[0].key,
+    });
 
-  // Get the URL of the uploaded file
-  const url = s3.getSignedUrl("getObject", {
-    Bucket: "connectImagesAndVideos",
-    Key: req.files[0].key,
-  });
-
-  // Return the URL
-  return {
-    url: url,
-  };
+    // Return the URL
+    return {
+      url: url,
+    };
+  }
+  catch (err) {
+    return null;
+  }
 }
-
 // IMPORTANT: Prevents next from trying to parse the form
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-interface ReturnData {
-  success: boolean;
-  httpResponse: string;
-  data?: string;
-}
 
 export default async function handler(
   req: NextApiRequest,
