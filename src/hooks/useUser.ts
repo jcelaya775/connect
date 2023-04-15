daf504492a4fe5805f9a0d999f017f4640e705d7
Jorge Celaya <jcelaya775@gmail.com>
2023-04-15 18:07:26 -0500

    Update useUser to use tanstack-query

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,18 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import { IUser } from "@/models/User";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 export default function useUser() {
-  const [user, setUser] = useState<IUser>();
-
-  useEffect(() => {
-    const getUser = async () => {
+  const {
+    isLoading,
+    error,
+    data: user,
+  } = useQuery<IUser>({
+    queryKey: ["user"],
+    queryFn: async () => {
       const { data } = await axios.get("/api/auth");
-      setUser(data.user);
-    };
-
-    getUser();
-  }, []);
+      return data.user;
+    },
+  });
 
   return user;
 }
