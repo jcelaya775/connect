8a909b2ed60934a93c436cdbcaf60de2d9161804
zachary belles <zmbelles@noctrl.edu>
2023-04-19 20:38:29 -0500

    change form file parse

--- src/pages/api/posts/image/index.ts
+++ src/pages/api/posts/image/index.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import connectDB from "@/lib/mongodb";
 import { getAuthUser } from "@/lib/auth";
-import { parseFormFile } from "@/lib/parse-form-file";
 import { uploadFileToS3 } from "@/lib/amazon-s3";
+import formidable from "formidable";
+import fs from "fs";
 
 // IMPORTANT: Prevents next from trying to parse the form
 export const config = {
@@ -10,7 +11,16 @@ export const config = {
     bodyParser: false,
   },
 };
-
+interface ParsedFile {
+  filename: string | null;
+  content: Buffer;
+}
+interface uploadFile {
+  success: boolean;
+  signedUrl?: string;
+  message: string;
+  key?: string;
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -25,24 +35,60 @@ export default async function handler(
       res.status(200).json({ success: true, httpStatus: "200", data: data });
     }
     case "POST": {
+      const form = new formidable.IncomingForm();
       try {
-        console.log(`pre parse`);
-        const file = await parseFormFile(req);
-        console.log(file.buffer)
-        if (!file || file === undefined) {
-          res.status(404).json({ success: false });
-          return;
+        const parsedFile = await new Promise<ParsedFile>((resolve, reject) => {
+          form.parse(req, (err, fields, files) => {
+            if (err) {
+              console.error(err);
+              reject(new Error("File upload failed"));
+              return;
+            }
+
+            const file = files["file"] as formidable.File;
+
+            if (!file) {
+              reject(new Error("File not found"));
+              return;
+            }
+
+            const content = fs.readFileSync(file.filepath);
+
+            if (!content) {
+              reject(new Error("Failed to read file content"));
+              return;
+            }
+
+            const parsedFile: ParsedFile = {
+              filename: file.originalFilename,
+              content: content,
+            };
+
+            resolve(parsedFile);
+          });
+        });
+
+        // Do something with the parsed file, e.g. upload to S3
+        if (!parsedFile.filename || !parsedFile.content) {
+          res
+            .status(400)
+            .json({ status: false, message: "File not parsed properly." });
         }
-        
-        const url = await uploadFileToS3(file);
-        if (url) {
-          res.status(200).json({ success: true, url: url });
-          return;
+
+        const signedURL = await uploadFileToS3(
+          parsedFile.content,
+          parsedFile.filename!
+        );
+        if (!signedURL) {
+          res
+            .status(400)
+            .json({ status: false, message: "could not upload to S3" });
         }
-        res.status(202).json({ success: true, url: null });
+        res.status(200).json({ status: true, key: parsedFile.filename, url: signedURL });
       } catch (err) {
-        console.log(`Something went wrong: ${err}`);
-        res.status(401).json({ body: `${err}` });
+        res
+          .status(401)
+          .json({ status: false, message: "could not parse file" });
       }
     }
   }
---
+++ src/pages/api/posts/index.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "@/lib/mongodb";
+import { getAuthUser } from "@/lib/auth";
+import { uploadFileToS3 } from "@/lib/amazon-s3";
+
+// IMPORTANT: Prevents next from trying to parse the form
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  await connectDB();
+
+  switch (method) {
+    case "GET": {
+      const data = req.query.data;
+      res.status(200).json({ success: true, httpStatus: "200", data: data });
+    }
+    case "POST": {
+      try {
+        
+      } catch (err) {
+        console.log(`Something went wrong: ${err}`);
+        res.status(401).json({ body: `${err}` });
+      }
+    }
+  }
+}
