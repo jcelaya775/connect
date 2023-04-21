b49817391b5b571f5385cc42e8b4e1ee0c1db30a
joshuaczajka <josh@jrcapps.com>
2023-04-20 21:31:26 -0500

    signup, 404 and settings pages

--- src/components/signup-box.tsx
+++ src/components/signup-box.tsx
@@ -63,89 +63,153 @@ const SignUpBox = () => {
 
   // TODO: Allow for username or email login
   return (
-    <div className={loginstyle.container}>
-      <form action="#" method="post">
-        <h1>
-          Connect
-          <Image className={loginstyle.icon} src={logo} alt=""></Image>
-        </h1>
-        <h3 className={loginstyle.tagline}>The Social Media Hub</h3>
-        <input
-          className={loginstyle.forminput}
-          type="text"
-          placeholder="First Name"
-          pattern="^[a-z '-]{1,30}+$/i"
-          required
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <br />
-        <input
-          className={loginstyle.forminput}
-          type="text"
-          placeholder="Last Name"
-          pattern="^[a-z '-]{1,30}+$/i"
-          required
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <br />
-        <input
-          className={loginstyle.forminput}
-          type="text"
-          placeholder="Username"
-          // TODO: Add validation for username
-          required
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          className={loginstyle.forminput}
-          type="email"
-          placeholder="Email Address"
-          pattern="^[\w.]+@([\w-]+.)+[\w-]{2,4}$"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          className={`${loginstyle.forminput} ${
-            formComplete &&
-            (passwordsMatch ? loginstyle.valid : loginstyle.invalid)
-          }`}
-          type="password"
-          placeholder="Password"
-          pattern="^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[*.!@$%^&(){}[]:;<>,.?/~_+-=|]).{8,32}$"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        {/* TODO: Give user feedback on password strength */}
-        <input
-          className={`${loginstyle.forminput} ${
-            formComplete &&
-            (passwordsMatch ? loginstyle.valid : loginstyle.invalid)
-          }`}
-          type="password"
-          placeholder="Confirm Password"
-          required
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <p className={loginstyle.message}>
-          Already have an account?&nbsp;
-          <strong>
-            <Link href="/login">Login</Link>
-          </strong>
-        </p>
-        <Link href="#">
-          <button
-            className={loginstyle.button}
-            onClick={signup}
-            disabled={!formComplete || !passwordsMatch}
-          >
-            Sign Up
-          </button>
-        </Link>
-      </form>
-    </div>
+    // <div className={loginstyle.container}>
+    //   <form action="#" method="post">
+    //     <h1>
+    //       Connect
+    //       <Image className={loginstyle.icon} src={logo} alt=""></Image>
+    //     </h1>
+    //     <h3 className={loginstyle.tagline}>The Social Media Hub</h3>
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="text"
+    //       placeholder="First Name"
+    //       pattern="^[a-z '-]{1,30}+$/i"
+    //       required
+    //       onChange={(e) => setFirstName(e.target.value)}
+    //     />
+    //     <br />
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="text"
+    //       placeholder="Last Name"
+    //       pattern="^[a-z '-]{1,30}+$/i"
+    //       required
+    //       onChange={(e) => setLastName(e.target.value)}
+    //     />
+    //     <br />
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="text"
+    //       placeholder="Username"
+    //       // TODO: Add validation for username
+    //       required
+    //       onChange={(e) => setUsername(e.target.value)}
+    //     />
+    //     <br />
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="email"
+    //       placeholder="Email Address"
+    //       pattern="^[\w.]+@([\w-]+.)+[\w-]{2,4}$"
+    //       required
+    //       onChange={(e) => setEmail(e.target.value)}
+    //     />
+    //     <br />
+    //     <input
+    //       className={`${loginstyle.forminput} ${
+    //         formComplete &&
+    //         (passwordsMatch ? loginstyle.valid : loginstyle.invalid)
+    //       }`}
+    //       type="password"
+    //       placeholder="Password"
+    //       pattern="^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[*.!@$%^&(){}[]:;<>,.?/~_+-=|]).{8,32}$"
+    //       required
+    //       onChange={(e) => setPassword(e.target.value)}
+    //     />
+    //     <br />
+    //     {/* TODO: Give user feedback on password strength */}
+    //     <input
+    //       className={`${loginstyle.forminput} ${
+    //         formComplete &&
+    //         (passwordsMatch ? loginstyle.valid : loginstyle.invalid)
+    //       }`}
+    //       type="password"
+    //       placeholder="Confirm Password"
+    //       required
+    //       onChange={(e) => setConfirmPassword(e.target.value)}
+    //     />
+    //     <p className={loginstyle.message}>
+    //       Already have an account?&nbsp;
+    //       <strong>
+    //         <Link href="/login">Login</Link>
+    //       </strong>
+    //     </p>
+    //     <Link href="#">
+    //       <button
+    //         className={loginstyle.button}
+    //         onClick={signup}
+    //         disabled={!formComplete || !passwordsMatch}
+    //       >
+    //         Sign Up
+    //       </button>
+    //     </Link>
+    //   </form>
+    // </div>
+
+    <div data-theme="corporate">
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row z-0">
+            <div className="text-center max-w-screen-sm prose lg:text-left lg:w-[500px]">
+              <h1 className="text-5xl font-bold">
+                Connect{" "}
+                <svg
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="fill-current w-[45px] h-[45px] inline pl-1"
+                >
+                  <path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"></path>
+                </svg>
+              </h1>
+              <p className="py-6 pl-0">
+                A new social media experience! Targeted towards content creators
+                and businesses to engage with their audiences across multiple
+                social media platforms.
+              </p>
+              <button
+                className="btn bg-primary hover:bg-secondary"
+              >
+                <Link href="/">Login</Link>
+              </button>
+            </div>
+            <div className="divider divider-vertical lg:hidden">OR Sign Up</div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <div className="card-body">
+                <h1 className="text-3xl font-bold text-center pb-2">Sign Up</h1>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered"
+                  />
+              </div>
+              <div className="form-control">
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button
+                    className="btn bg-primary hover:bg-secondary"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
   );
 };
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,16 +2,40 @@ import style from "@/styles/FormBox.module.css";
 import Header from "@/components/header";
 import Image from "next/image";
 import logo from "@/images/link_icon_content.svg";
+import Link from "next/link"
 
 export default function NotFound() {
   return (
     <>
-      <div className={style.container}>
-        <h1>
-          Connect
-          <Image className={style.icon} src={logo} alt=""></Image>
-          &nbsp;: Page Not Found
-        </h1>
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-gray-100 p-6 m-8 rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2">
+          <div className="flex flex-col sm:flex-row sm:gap-x-4">
+            <div className="flex w-full sm:w-1/4 xl:1/6">
+              <svg xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="text-error w-1/3 h-1/3 mx-auto sm:mx-0 sm:w-full sm:h-full"
+              >
+                <path strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                />
+              </svg>
+            </div>
+            <div className="flex-1 w-full">
+              <h1 className="text-2xl text-error font-bold mb-4">404 Error: Page not found</h1>
+              <p className="text-gray-600">We apologize for the inconvenience. The page you were looking for doesn't exist or is currently undergoing
+                maitenance. Please also double check your URL to make sure there were no errors as this may have also caused this issue.</p>
+              <p className="text-primary text-right pt-2">
+                <Link href="/" className="hover:underline">
+                  Return to Homepage
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
