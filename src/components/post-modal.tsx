ea4c356057e24f212ff4dab57e30d126fe37670a
Jorge Celaya <jcelaya775@gmail.com>
2023-04-20 20:30:15 -0500

    Add connect option back

--- src/components/post-modal.tsx
+++ src/components/post-modal.tsx
@@ -66,6 +66,7 @@ const PostModal = ({ setVisible, newPost = true }: PostModalProps) => {
 
   const postToConnect = async (postData: any) => {
     const res = await axios.post("/api/posts", postData);
+    console.log(`Posting to Connect: ${JSON.stringify(postData)}`);
 
     if (res.data.success === false) {
       throw new Error("Error posting to Connect");
