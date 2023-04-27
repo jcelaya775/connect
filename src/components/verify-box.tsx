f9263f90f65adb5a1e819034385b94a954eaf8c2
joshuaczajka <josh@jrcapps.com>
2023-04-27 10:32:27 -0500

    update to verify-email page

--- src/components/verify-box.tsx
+++ src/components/verify-box.tsx
@@ -54,36 +54,85 @@ const VerifyBox = () => {
 
   // TODO: Add input validation
   return (
-    <div className={loginstyle.container}>
-      <form action="#" method="post">
-        <h1>
-          Connect
-          <Image className={loginstyle.icon} src={logo} alt=""></Image>
-        </h1>
-        <h3 className={loginstyle.tagline}>The Social Media Hub</h3>
-        <p className={loginstyle.message}>
-          To complete registration enter your email address and verification
-          code.
-        </p>
-        <input
-          className={loginstyle.forminput}
-          type="email"
-          placeholder="Email Address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          className={loginstyle.forminput}
-          type="text"
-          placeholder="Verification Code"
-          onChange={(e) => setVerificationCode(e.target.value)}
-        />
-        <br />
-        <button className={loginstyle.button} onClick={verify}>
-          Verify Account
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-xl">
+            <h1 className="text-5xl font-bold">
+              Connect{" "}
+              <svg
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                className="fill-current w-[45px] h-[45px] inline pl-1"
+              >
+                <path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"></path>
+              </svg>
+            </h1>
+            <p className="py-6">To complete registration enter your email address and verification code.</p>
+            <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+              <div className="card-body">
+                <form action="#" method="post">
+                  <div className="form-control">
+                    <input 
+                      type="email"
+                      placeholder="email"
+                      className="input input-bordered mb-4"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-control">
+                    <input 
+                      type="text"
+                      placeholder="verification code"
+                      className="input input-bordered"
+                      onChange={(e) => setVerificationCode(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    className="btn btn-primary mt-6 normal-case"
+                    onClick={verify}
+                  >
+                    Verify Code
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </>
+
+
+    // <div className={loginstyle.container}>
+    //   <form action="#" method="post">
+    //     <h1>
+    //       Connect
+    //       <Image className={loginstyle.icon} src={logo} alt=""></Image>
+    //     </h1>
+    //     <h3 className={loginstyle.tagline}>The Social Media Hub</h3>
+    //     <p className={loginstyle.message}>
+    //       To complete registration enter your email address and verification
+    //       code.
+    //     </p>
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="email"
+    //       placeholder="Email Address"
+    //       onChange={(e) => setEmail(e.target.value)}
+    //     />
+    //     <br />
+    //     <input
+    //       className={loginstyle.forminput}
+    //       type="text"
+    //       placeholder="Verification Code"
+    //       onChange={(e) => setVerificationCode(e.target.value)}
+    //     />
+    //     <br />
+    //     <button className={loginstyle.button} onClick={verify}>
+    //       Verify Account
+    //     </button>
+    //   </form>
+    // </div>
   );
 };
 
